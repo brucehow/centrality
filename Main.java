456d6ff8867f9e379dd876f4ec373aa4812536c5
Bruce How <brucehow4@gmail.com>
2018-05-26 21:20:22 +0800

    Add readable result output to code

--- Main.java
+++ Main.java
@@ -1,43 +1,72 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		String filepath = "/Users/bruce/Documents/workspace/Project2018/src/78813.edges";
-		Graph g = new Graph(filepath);
+		Scanner sc = new Scanner(System.in);
+		/////////////////////////////////////////////////////////////////////////////
+		// File Predefined Directories - Change this to your own project directory //
+		/////////////////////////////////////////////////////////////////////////////
+		String dir = "/Users/bruce/Documents/workspace/Project2018/src/";
+
+		System.out.print("Enter edge file name: ");
+		String filepath = dir + sc.next() + ".edges";
+		Graph g;
+		try {
+			g = new Graph(filepath);
+		} catch (FileNotFoundException e) {
+			System.err
+					.println("Invalid file. Please check file name and predefined dir.");
+			return;
+		}
 		Centrality c = new Centrality();
-		//System.out.println("Node count : " + g.getNodes().size());
+		int count = 1;
+		/** Get Node Sizes */
+		// System.out.println("Node count : " + g.getNodes().size());
 
-		// Test connected Nodes
-		//System.out.println(g.getConnectedNodes(759251) + "\nCount: " + g.getConnectedNodes(759251).size());
-		
-		// Test toString
-		//System.out.println(g.toString());
-	
-		// Test Degree
-		//System.out.println(Arrays.toString(c.degreeCentrality(g)));
-		
-		// Test components
-		/*Iterator<Integer> it = g.getNodes().iterator();
-		for (ArrayList<Integer> comp : g.getComponents()) {
-			System.out.println(comp);
-			int count = 0;
-			for (Integer x : comp) {
-				count++;
+		/** Test Graph.getConnectedNodes() */
+		// System.out.println(g.getConnectedNodes(759251) + "\nCount: " +
+		// g.getConnectedNodes(759251).size());
+
+		/** Test Graph.toString() */
+		// System.out.println(g.toString());
+
+		/** Test Graph.getComponents() */
+		/*
+		 * Iterator<Integer> it = g.getNodes().iterator(); for (ArrayList<Integer> comp :
+		 * g.getComponents()) { System.out.println(comp); int count = 0; for (Integer x :
+		 * comp) { count++; } System.out.println("Count : " + count); }
+		 */
+		for (int i = 0; i < g.getComponents().size(); i++) {
+			System.out.println("\nComponent " + (i+1) + "\n------------------------");
+			/////////////////////////////////////////////////////////////////////////////
+			/////////////////////////// Degree Centrality Test //////////////////////////
+			/////////////////////////////////////////////////////////////////////////////
+			count = 1;
+			System.out.println("Degree Centrality:");
+			for (Integer node : c.degreeCentrality(g)[i]) {
+				System.out.println(node);
 			}
-			System.out.println("Count : " + count);
-		}*/
-		
-		// Closeness Test
-		//System.out.println(Arrays.toString(c.closenessCentrality(g)));
-		
-		//Betweenness Test
-		System.out.println(Arrays.toString(c.betweennessCentrality(g)));
+
+			/////////////////////////////////////////////////////////////////////////////
+			////////////////////////// Closeness Centrality Test ////////////////////////
+			/////////////////////////////////////////////////////////////////////////////
+			count = 1;
+			System.out.println("Closness Centrality:");
+			for (Integer node : c.closenessCentrality(g)[i]) {
+				System.out.println(node);
+			}
+
+			/////////////////////////////////////////////////////////////////////////////
+			//////////////////////// Betweenness Centrality Test ////////////////////////
+			/////////////////////////////////////////////////////////////////////////////
+			count = 1;
+			System.out.println("Betweeness Centrality:");
+			for (Integer node : c.betweennessCentrality(g)[i]) {
+				System.out.println(node);
+			}
+		}
 	}
-
-
 }
