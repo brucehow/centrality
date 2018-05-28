07e748c9191f15bcd2d4b33d44b1e4ad66ffc1b3
Bruce How <brucehow4@gmail.com>
2018-05-28 17:18:08 +0800

    Add Katz Centrality & Documentation

--- Centrality.java
+++ Centrality.java
@@ -5,17 +5,21 @@ import java.util.Queue;
 import java.util.Stack;
 
 /**
- * <description>
+ * A class containing several methods to compute the centrality of a given
+ * graph. This includes degree centrality, closeness centrality, betweenness
+ * centrality and Katz centrality. This class utilizes the graph class'
+ * representation of a graph.
  * @author Bruce How (22242664) & Haolin Wu (21706137)
  */
 public class Centrality {
 
 	/**
-	 * Returns an array of integer ArrayList, containing the nodes with the highest
-	 * degree centrality, for each component(s). This is the node which has the highest
-	 * number of incident edges.
+	 * Returns an array of integer ArrayList, containing the nodes with the
+	 * highest degree centrality, for each component of the graph. This is the
+	 * node which has the highest number of incident edges.
 	 * @param graph The graph to be checked
-	 * @return An array of nodes with the highest degree centrality for each component.
+	 * @return An array of nodes with the highest degree centrality for each
+	 *         component
 	 */
 	public ArrayList<Integer>[] degreeCentrality(Graph graph) {
 		int componentSize = graph.getComponents().size();
@@ -41,12 +45,12 @@ public class Centrality {
 	}
 
 	/**
-	 * Performs a BFS algorithm on a given graph to fetch the nodes with the highest
-	 * closeness centrality for each component(s). This is the node with the smallest
-	 * average shortest paths to all other nodes in the graph.
+	 * Performs a BFS on a given graph to fetch the nodes with the highest
+	 * closeness centrality for each component of the graph. This is the node
+	 * with the smallest average shortest paths to all other nodes in the graph.
 	 * @param graph The graph to be checked
 	 * @return An array of nodes with the highest closeness centrality for each
-	 *         component.
+	 *         component
 	 */
 	public ArrayList<Integer>[] closenessCentrality(Graph graph) {
 		int componentSize = graph.getComponents().size();
@@ -56,8 +60,8 @@ public class Centrality {
 			ArrayList<Integer> component = graph.getComponents().get(i);
 			for (Integer source : component) {
 				/*
-				 * Runs a BFS algorithm on the graph to find the shortest distances to
-				 * all other nodes from the current source node
+				 * Runs a BFS algorithm on the graph to find the shortest
+				 * distances to all other nodes from the current source node
 				 */
 				HashMap<Integer, Integer> distance = new HashMap<>();
 				Queue<Integer> queue = new LinkedList<>();
@@ -77,8 +81,9 @@ public class Centrality {
 				}
 
 				/*
-				 * Caculate total distance for the SP of the source to each node and
-				 * update the variables respectively
+				 * Caculate total distance for the SP of the source to each node
+				 * and update the variables respectively. This is the far(j) as
+				 * described in the project brief
 				 */
 				int totalDistance = 0;
 				for (Integer nodeDistance : distance.values()) {
@@ -89,7 +94,8 @@ public class Centrality {
 					ArrayList<Integer> results = new ArrayList<>();
 					results.add(source);
 					closeness[i] = results;
-				} else if (totalDistance == shortest && closeness[i].size() < 5) {
+				} else if (totalDistance == shortest
+						&& closeness[i].size() < 5) {
 					ArrayList<Integer> results = closeness[i];
 					results.add(source);
 					closeness[i] = results;
@@ -100,9 +106,9 @@ public class Centrality {
 	}
 
 	/**
-	 * Performs Brandes algorithm on a given graph to fetch the nodes with the highest
-	 * bewtweenness centrality for each component(s). This is the node which passes
-	 * through the most shortest paths in a graph
+	 * Performs Brandes algorithm on a given graph to fetch the nodes with the
+	 * highest bewtweenness centrality for each component of the graph. This is
+	 * the node which passes through the most shortest paths in a graph
 	 * @param graph The graph to be checked
 	 * @return An array of nodes with the highest closeness centrality for each
 	 *         component.
@@ -119,9 +125,9 @@ public class Centrality {
 
 			for (Integer source : component) {
 				/*
-				 * Runs a BFS algorithm on the graph to find the shortest distances to
-				 * all other nodes, preceding nodes that pass within all the SP, and
-				 * number of SP from the source node
+				 * Runs a BFS algorithm on the graph to find the shortest
+				 * distances to all other nodes, preceding nodes that pass
+				 * within all the SP, and number of SP from the source node
 				 */
 				Stack<Integer> stack = new Stack<>();
 				Queue<Integer> queue = new LinkedList<>();
@@ -149,17 +155,20 @@ public class Centrality {
 							parent.add(current);
 							precedingNode.put(node, parent);
 							// Found another SP for existing node
-						} else if (distance.get(node) == distance.get(current) + 1) {
-							paths.put(node, paths.get(node) + paths.get(current));
-							ArrayList<Integer> precede = precedingNode.get(node);
+						} else if (distance.get(node) == distance.get(current)
+								+ 1) {
+							paths.put(node,
+									paths.get(node) + paths.get(current));
+							ArrayList<Integer> precede = precedingNode
+									.get(node);
 							precede.add(current);
 							precedingNode.put(node, precede);
 						}
 					}
 				}
 				/*
-				 * Runs Brandes' dependency accumulation algorithm to compute the
-				 * betweenness centrality for each node.
+				 * Runs Brandes' dependency accumulation algorithm to compute
+				 * the betweenness centrality for each node.
 				 */
 				HashMap<Integer, Double> dependency = new HashMap<>();
 				for (Integer node : component) {
@@ -169,21 +178,23 @@ public class Centrality {
 					int current = stack.pop();
 					if (current != source) {
 						for (Integer node : precedingNode.get(current)) {
-							double result = ((double) paths.get(node) / paths.get(current))
+							double result = ((double) paths.get(node)
+									/ paths.get(current))
 									* (1 + dependency.get(current));
 							dependency.put(node, dependency.get(node) + result);
 						}
 						if (!centrality.containsKey(current)) {
 							centrality.put(current, dependency.get(current));
 						} else {
-							centrality.put(current, centrality.get(current) + dependency.get(current));
+							centrality.put(current, centrality.get(current)
+									+ dependency.get(current));
 						}
 					}
 				}
 			}
 			/*
-			 * Identify the node(s) with the highest centrality value and update the
-			 * betweenness ArrayList array appropiately.
+			 * Identify the node(s) with the highest centrality value and update
+			 * the betweenness ArrayList array appropiately.
 			 */
 			double minDependency = -1;
 			for (Integer node : centrality.keySet()) {
@@ -192,26 +203,78 @@ public class Centrality {
 					ArrayList<Integer> results = new ArrayList<>();
 					results.add(node);
 					betweenness[i] = results;
-				} else if (centrality.get(node) == minDependency && betweenness[i].size() < 5) {
+				} else if (centrality.get(node) == minDependency
+						&& betweenness[i].size() < 5) {
 					ArrayList<Integer> results = betweenness[i];
 					results.add(node);
 					betweenness[i] = results;
 				}
 			}
+			System.out.println(centrality.toString());
 		}
 		return betweenness;
 	}
 
 	/**
-	 * TODO desc of katzCentrality method
+	 * Performs a BFS on a given graph to fetch the nodes with the highest Katz
+	 * centrality for each component of the graph. This is the node which has
+	 * the highest overall number of edges, that are close by.
 	 * @param graph The graph to be checked
-	 * @return TODO return info
+	 * @return An array of nodes with the highest Katz centrality for each
+	 *         component
 	 */
-	public ArrayList<Integer>[] katzCentralitry(Graph graph, int alpha) {
+	public ArrayList<Integer>[] katzCentralitry(Graph graph, double alpha) {
 		int componentSize = graph.getComponents().size();
 		ArrayList<Integer>[] katz = new ArrayList[componentSize];
 
+		for (int i = 0; i < componentSize; i++) {
+			double highest = 0;
+			ArrayList<Integer> component = graph.getComponents().get(i);
+
+			for (Integer source : component) {
+				/*
+				 * Runs a BFS algorithm on the graph to identify the node level
+				 * of each node form the source and compute the Katz centrality
+				 * value for the source
+				 */
+				HashMap<Integer, Integer> level = new HashMap<>();
+				Queue<Integer> queue = new LinkedList<>();
+				level.put(source, 0);
+
+				// Stores the Katz centrality value of the source node
+				double centrality = 0;
+				int current = source;
+
+				while (level.size() != component.size()) {
+					for (Integer node : graph.getConnectedNodes(current)) {
+						if (!level.containsKey(node)) {
+							int newLevel = level.get(current) + 1;
+							centrality += Math.pow(alpha, newLevel);
+							level.put(node, newLevel);
+							queue.add(node);
+						}
+					}
+					if (!queue.isEmpty()) {
+						current = queue.poll();
+					}
+				}
+				/*
+				 * Identify whether the current source has the highest Katz
+				 * centrality value and update the katz ArrayList array
+				 * appropiately.
+				 */
+				if (centrality > highest) {
+					ArrayList<Integer> results = new ArrayList<>();
+					results.add(source);
+					highest = centrality;
+					katz[i] = results;
+				} else if (centrality == highest && katz[i].size() < 5) {
+					ArrayList<Integer> results = katz[i];
+					results.add(source);
+					katz[i] = results;
+				}
+			}
+		}
 		return katz;
 	}
-
 }
