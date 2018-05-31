59c0d825bddab6f784a13357033b627e46d3ea2e
Bruce How <brucehow4@gmail.com>
2018-05-31 17:47:57 +0800

    Change code to work with stdin

--- Main.java
+++ Main.java
@@ -1,76 +1,56 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
-	/////////////////////////////////////////////////////////////////////////////
-	// File Predefined Directories - Change this to your own project directory //
-	/////////////////////////////////////////////////////////////////////////////
-	static String dir = "/Users/bruce/Documents/workspace/Project2018/src/";
+
+	private static String filepath;
+	private static Graph graph;
+	private static Centrality centrality;
+	private static double alpha;
 
 	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
-
-		System.out.print("Enter edge file name: ");
-		String filepath = dir + sc.next() + ".edges";
-		Graph g;
-		try {
-			g = new Graph(filepath);
-		} catch (FileNotFoundException e) {
-			System.err.println("Invalid file. Please check file name and predefined dir.");
+		if (args.length != 1 && args.length != 2) {
+			System.err.println("Invalid Arguments\n\tUsage: java Main <filepath>");
+			System.err.println(
+					"\nCustom Katz alpha value\n\tUsage: java Main <filepath> <alpha>");
 			return;
 		}
-		Centrality c = new Centrality();
-		int count = 1;
-		/** Get Node Sizes */
-		// System.out.println("Node count : " + g.getNodes().size());
+		if (args.length == 2) {
+			alpha = Double.parseDouble(args[1]);
+		} else {
+			alpha = 0.5;
+		}
+		filepath = args[0];
+		try {
+			graph = new Graph(filepath);
+		} catch (FileNotFoundException e) {
+			System.err.println("File not found\nPlease check the provided file path");
+			return;
+		}
 
-		/** Test Graph.getConnectedNodes() */
-		// System.out.println(g.getConnectedNodes(759251) + "\nCount: " +
-		// g.getConnectedNodes(759251).size());
+		centrality = new Centrality();
 
-		/** Test Graph.toString() */
-		// System.out.println(g.toString());
+		System.out.println(filepath);
 
-		/** Test Graph.getComponents() */
-		/*
-		 * Iterator<Integer> it = g.getNodes().iterator(); for (ArrayList<Integer> comp :
-		 * g.getComponents()) { System.out.println(comp); int count = 0; for (Integer x :
-		 * comp) { count++; } System.out.println("Count : " + count); }
-		 */
-		/*
-		for (int i = 0; i < g.getComponents().size(); i++) {
-			System.out.println("\nComponent " + (i + 1) + "\n-----------------");
-			/////////////////////////////////////////////////////////////////////////////
-			/////////////////////////// Degree Centrality Test //////////////////////////
-			/////////////////////////////////////////////////////////////////////////////
-			count = 1;
-			System.out.println("Degree Centrality:");
-			for (Integer node : c.degreeCentrality(g)[i]) {
-				System.out.println(node);
+		for (int i = 0; i < graph.getComponents().size(); i++) {
+			System.out.println("==================");
+			System.out.println("Component #" + (i + 1));
+			System.out.println("Degree centers:");
+			for (Integer node : centrality.degreeCentrality(graph).get(i)) {
+				System.out.println("\t" + node);
 			}
-
-			/////////////////////////////////////////////////////////////////////////////
-			////////////////////////// Closeness Centrality Test ////////////////////////
-			/////////////////////////////////////////////////////////////////////////////
-			count = 1;
-			System.out.println("Closness Centrality:");
-			for (Integer node : c.closenessCentrality(g)[i]) {
-				System.out.println(node);
+			System.out.println("Closeness centers:");
+			for (Integer node : centrality.closenessCentrality(graph).get(i)) {
+				System.out.println("\t" + node);
 			}
-
-			/////////////////////////////////////////////////////////////////////////////
-			//////////////////////// Betweenness Centrality Test ////////////////////////
-			/////////////////////////////////////////////////////////////////////////////
-			count = 1;
-			System.out.println("Betweeness Centrality:");
-			ArrayList<Integer>[] ans = c.betweennessCentrality(g);
-			for (Integer node : ans[i]) {
-				System.out.println(node);
+			System.out.println("Betweenness centers:");
+			for (Integer node : centrality.betweennessCentrality(graph).get(i)) {
+				System.out.println("\t" + node);
 			}
-		}*/
-		c.betweennessCentrality(g);
-		sc.close();
+			System.out.println("Katz centers (a = " + alpha + "):");
+			for (Integer node : centrality.katzCentrality(graph, alpha).get(i)) {
+				System.out.println("\t" + node);
+			}
+		}
 	}
 }
